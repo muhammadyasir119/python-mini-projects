ac52e9bec8ff8c0c9a18e8c08847224d047c5e57
Muhammad yasir <yasirrashid125@gmail.com>
2026-07-15 17:17:52 +0500

    Add variables and user input example in Python

---
+++ python-basics/week-1/datatypes/variables.py
@@ -0,0 +1,20 @@
+# Day 2 - Variables & Input
+
+# Variables
+name = "Yasir"
+age = 20
+
+print("My name is:", name)
+print("My age is:", age)
+
+# Taking input from user
+user_name = input("Enter your name: ")
+print("Hello", user_name)
+
+# Type Casting
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("Sum is:", sum)
